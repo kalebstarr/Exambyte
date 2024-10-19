faa06fd06d5a6578d3380618b705b9ea2080de1b
Kaleb Starr <kaleb.starr@proton.me>
2024-10-19 18:17:13 +0200

    Add initial fragments for later templating.

--- src/main/java/com/soup/exambyte/controllers/UserController.java
+++ src/main/java/com/soup/exambyte/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.soup.exambyte.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -16,7 +17,8 @@ public class UserController {
    * @return index.html template.
    */
   @GetMapping("/")
-  public String index() {
+  public String index(Model model) {
+    model.addAttribute("title", "Home");
     return "index";
   }
 }
