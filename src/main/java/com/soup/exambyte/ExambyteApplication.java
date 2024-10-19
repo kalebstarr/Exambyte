79c126e764aa2f3eb0aa91176e6887f27c992bcb
Kaleb Starr <kaleb.starr@proton.me>
2024-10-19 16:30:01 +0200

    Initial commit

---
+++ src/main/java/com/soup/exambyte/ExambyteApplication.java
@@ -0,0 +1,13 @@
+package com.soup.exambyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExambyteApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExambyteApplication.class, args);
+  }
+
+}
