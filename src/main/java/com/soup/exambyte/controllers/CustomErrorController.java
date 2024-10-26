d475ddf622820dbd27235b0da18ead0c8db8a981
Kaleb Starr <kaleb.starr@hhu.de>
2024-10-26 16:46:06 +0200

    Refactor CustomErrorController to follow styleguide and add javadoc

--- src/main/java/com/soup/exambyte/controllers/CustomErrorController.java
+++ src/main/java/com/soup/exambyte/controllers/CustomErrorController.java
@@ -9,9 +9,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+/**
+ * Controller for error requests and error routing for other controllers.
+ */
 @Controller
 public class CustomErrorController implements ErrorController {
 
+  /**
+   * Handles requests for errors ("/error")
+   * Specifically validates NOT_FOUND and INTERNAL_SERVER_ERROR and redirects error status to view.
+
+   * @param model   The spring model.
+   * @param request HttpServletRequest to get HttpStatus of request.
+   * @return        Returns the "error" template.
+   */
   @RequestMapping("/error")
   public String errorView(Model model, HttpServletRequest request) {
     Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
@@ -20,13 +31,11 @@ public class CustomErrorController implements ErrorController {
       model.addAttribute("title", "Error");
       int statusCode = Integer.parseInt(status.toString());
 
-      if(statusCode == HttpStatus.NOT_FOUND.value()) {
+      if (statusCode == HttpStatus.NOT_FOUND.value()) {
         model.addAttribute("error", HttpStatus.NOT_FOUND.value());
-      }
-      else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+      } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
         model.addAttribute("error", HttpStatus.INTERNAL_SERVER_ERROR.value());
-      }
-      else {
+      } else {
         model.addAttribute("error", statusCode);
       }
     } else {
