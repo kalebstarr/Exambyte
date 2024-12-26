921307cc22b61b48974474b5ffdf31840bf4af2b
Kaleb Starr <kaleb.starr@hhu.de>
2024-12-26 17:52:20 +0100

    Add initial Security Config

---
+++ src/main/java/com/soup/exambyte/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.soup.exambyte.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity chainBuilder) throws Exception {
+    chainBuilder.authorizeHttpRequests(
+            configurer -> configurer.requestMatchers("/", "/css/*").permitAll()
+                .anyRequest().authenticated())
+            .oauth2Login(Customizer.withDefaults());
+    return chainBuilder.build();
+  }
+}
