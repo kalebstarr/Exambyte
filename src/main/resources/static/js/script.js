8eef2cd115c7af11b179191232e6137bfc587a2a
Kaleb Starr <kaleb.starr@hhu.de>
2024-12-30 15:10:44 +0100

    Add dynamic adding of options for multiple choice questions

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -12,14 +12,36 @@ document.addEventListener('DOMContentLoaded', function () {
         additionalContent.id = 'additionalContent';
         additionalContent.innerHTML = `
                 <hr>
-                <div class="row mb-3">
-                  <label for="option1" class="col-sm-2 col-form-label">Option 1</label>
-                  <div class="col-sm-10">
-                    <input type="text" class="form-control" id="option1" name="option1" placeholder="Option 1" required />
+                <div id="optionContainer">
+                  <div class="row mb-3">
+                    <label for="option1" class="col-sm-2 col-form-label">Option 1</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="option1" name="option1" placeholder="Option 1" required />
+                    </div>
                   </div>
                 </div>
+                <a id="add-option" class="btn btn-success" href="#">Add option</a>
               `;
         questionContainer.appendChild(additionalContent);
+
+        const addOption = document.getElementById('add-option');
+        const optionContainer = document.getElementById('optionContainer')
+        let optionCount = 1;
+
+        addOption.addEventListener('click', function (event) {
+          event.preventDefault();
+          optionCount++;
+
+          const newOption = document.createElement('div');
+          newOption.classList.add('row', 'mb-3')
+          newOption.innerHTML = `
+                  <label for="option${optionCount}" class="col-sm-2 col-form-label">Option ${optionCount}</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="option${optionCount}" name="option${optionCount}" placeholder="Option ${optionCount}" required />
+                  </div>
+               `;
+          optionContainer.appendChild(newOption);
+        })
       }
     } else {
       if (additionalContent) {
